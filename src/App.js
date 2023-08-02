01cf835defdebdf3516e2885bc0f9f786d3b5701
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-02 16:53:41 +0700

    Update https for fetch request

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard'
 import './App.css'
 import searchIcon from './search.svg'
 
-const API_URL = 'http://www.omdbapi.com?apikey=8fa9ada3'
+const API_URL = 'https://www.omdbapi.com?apikey=8fa9ada3'
 
 const App = () => {
     const [movies, setMovies] = useState([])
